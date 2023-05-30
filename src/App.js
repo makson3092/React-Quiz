54a525ffde7de9168b000f0dbf4a7056ea9d935b
makson3092 <makson3092@gmail.com>
2023-05-31 00:50:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { quiz } from "./assets/quiz";
+import win from "./assets/img/win-color.jpg";
+import soBad from "./assets/img/so-bad.jpg";
+import notBad from "./assets/img/not-bad.png";
+
+const Winner = ({ correct }) => {
+  if (correct === quiz.length) {
+    return <img src={win} alt="win" />;
+  } else if (correct >= 1) {
+    return (
+      <>
+        <img src={notBad} alt="notBad" />
+        <h2>Не погано, але можна краще!</h2>
+      </>
+    );
+  }
+  return (
+    <>
+      <img src={soBad} alt="soBad" />
+    </>
+  );
+};
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <Winner correct={correct} />
+      <h2>
+        Ви відповіли вірно {correct} з {quiz.length}
+      </h2>
+      <a href="/">
+        <button>Спробувати ще раз</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVersion }) {
+  const percent = Math.round((step / quiz.length) * 100);
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percent}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.version.map((text, index) => (
+          <li key={text} onClick={() => onClickVersion(index)}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = quiz[step];
+
+  const onClickVersion = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+    if (index === question.choice) {
+      setCorrect(correct + 1);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {step !== quiz.length ? (
+        <Game step={step} question={question} onClickVersion={onClickVersion} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
---
+++ src/assets/quiz.js
@@ -0,0 +1,23 @@
+export const quiz = [
+  {
+    title: "В якій директорії Git зберігає свої файли?",
+    version: [".repository", ".log", ".git", "history"],
+    choice: 2,
+  },
+  {
+    title: "Яка команда використовується для створення репозиторія ?",
+    version: ["git init", "git new", "git create", "git add"],
+    choice: 0,
+  },
+  {
+    title: "Яка площа України?",
+    version: ["600 400 км²", "603 700 км²", "551 695 км²", "613 700 км²"],
+    choice: 1,
+  },
+  {
+    title:
+      "Як називається об'єктна модель документа, яка будується на основі HTML кода ?",
+    version: ["ENV", "DOM", "Оточення", "BOM"],
+    choice: 1,
+  },
+];
